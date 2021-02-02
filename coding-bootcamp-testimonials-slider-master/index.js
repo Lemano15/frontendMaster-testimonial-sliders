0edfad6496e41368ad78131b6a9990195bdfb64f
Luke Emano <55148557+Lemano15@users.noreply.github.com>
2021-02-01 19:59:45 -0600

    Finalization

---
+++ coding-bootcamp-testimonials-slider-master/index.js
@@ -0,0 +1,33 @@
+let nextBtn = document.getElementById('nextButton');
+let prevBtn = document.getElementById('prevButton');
+let active = document.querySelector('.pageActive')
+let carouselItems = document.getElementsByClassName('carousel-item')
+let pages = document.getElementsByClassName('page')
+let pagePosition = 0;
+
+function nextSlide() {
+    carouselItems[pagePosition].classList.remove('pageActive');
+    pages[pagePosition].classList.remove('pageActive')
+    if (pagePosition == carouselItems.length - 1) {
+        pagePosition = 0;
+    } else {
+        pagePosition++;
+    }
+    carouselItems[pagePosition].classList.add('pageActive')
+    pages[pagePosition].classList.add('pageActive')
+}
+
+function previousSlide() {
+    carouselItems[pagePosition].classList.remove('pageActive');
+    pages[pagePosition].classList.remove('pageActive')
+    if (pagePosition == 0) {
+        pagePosition = carouselItems.length - 1;
+    }
+    carouselItems[pagePosition].classList.add(pageActive);
+    pages[pagePosition].classList.add('pageActive');
+}
+
+
+prevBtn.addEventListener('click', previousSlide)
+
+nextBtn.addEventListener("click", nextSlide)
